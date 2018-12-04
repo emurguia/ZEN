a92c63e975643ce02364ac614e17e3218e9c9c15
zoeg21 <zoeg21@gmail.com>
2018-12-03 22:24:22 -0500

    fixed circle function in VM

--- make_circle.c
+++ make_circle.c
@@ -2,15 +2,24 @@
 
 void make_circle(float x, float y, float radius, int vertices)
 {
-        slWindow(500, 500, "circle", false);
+        slWindow(500, 500, "circle", 0);
         slSetBackColor(1.0, 1.0, 1.0);
 
         while(!slShouldClose() && !slGetKey(SL_KEY_ESCAPE))
         {
-            slSetForeColor(0.0, 0.0, 0.0, 0.0);
+            slSetForeColor(0.0, 0.0, 0.0, 0.5);
+	    slCircleFill(x, y, radius, vertices);
+	    slSetForeColor(0.0, 0.0, 0.8, 0.8);
             slCircleOutline(x, y, radius, vertices);
+
 
             slRender();
         }
         slClose();
+}
+
+int main()
+{
+	make_circle(325.0, 450.0, 25.0, 16);
+	return 1;
 }
